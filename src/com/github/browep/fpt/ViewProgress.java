2d5c61349b6bcfbe1070d312362e087d30ffc92b
browep <brower.paul@gmail.com>
2011-03-22 23:12:33 -0400

    view progress changes

--- src/com/github/browep/fpt/ViewProgress.java
+++ src/com/github/browep/fpt/ViewProgress.java
@@ -22,61 +22,62 @@ import java.util.*;
  * Time: 11:19 PM
  * To change this template use File | Settings | File Templates.
  */
-public class ViewProgress extends DaoAwareActivity{
-    Integer workoutDefinitionId;
-    WorkoutDefinition definition;
-    private XYPlot mySimpleXYPlot;
+public class ViewProgress extends DaoAwareActivity {
+  Integer workoutDefinitionId;
+  WorkoutDefinition definition;
+  private XYPlot mySimpleXYPlot;
 
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.view_progress);
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.view_progress);
 
-        mySimpleXYPlot = (XYPlot) findViewById(R.id.mySimpleXYPlot);
+    mySimpleXYPlot = (XYPlot) findViewById(R.id.mySimpleXYPlot);
 
-        workoutDefinitionId = getIntent().getExtras().getInt(C.WORKOUT_DEFINITION_ID);
-        definition = (WorkoutDefinition) dao.get(workoutDefinitionId);
+    workoutDefinitionId = getIntent().getExtras().getInt(C.WORKOUT_DEFINITION_ID);
+    definition = (WorkoutDefinition) dao.get(workoutDefinitionId);
 
-        List<Number> yValues = new LinkedList<Number>();
-        List<Number> xValues = new LinkedList<Number>();
+    List<Number> yValues = new LinkedList<Number>();
+    List<Number> xValues = new LinkedList<Number>();
 
-        Map where = new HashMap();
-        where.put(C.WORKOUT_DEFINITION_ID,workoutDefinitionId.toString());
-        List<Storable> entries = dao.where(where);
+    Map model = getViewService().getPropertyDefinition((Integer) definition.get(C.WORKOUT_TYPE));
 
-        String xValuePropName = C.WORKOUT_TYPE_TO_X_PROP_NAME.get(definition.getType());
+    Map where = new HashMap();
+    where.put(C.WORKOUT_DEFINITION_ID, workoutDefinitionId.toString());
+    List<Storable> entries = dao.where(where);
+
+    String xValuePropName = (String) model.get("x_prop_name");
 //        if(C.WORKOUT_TYPE_TO_X_FORMAT.get(definition.getType())!=null)
 //            mySimpleXYPlot.setRangeValueFormat();
 
-        for(Storable storable : entries){
-            Workout workout = (Workout) storable;
-            yValues.add((Number) workout.get(xValuePropName));
-            xValues.add(workout.getCreated().getTime());
-        }
-
-        
-        mySimpleXYPlot.setBackgroundColor(Color.TRANSPARENT);
-
-        mySimpleXYPlot.setDrawBorderEnabled(false);
-
-        XYSeries series1 = new SimpleXYSeries(
-                xValues,
-                yValues,
-                "");
-                                 // Create a formatter to use for drawing a series using LineAndPointRenderer:
-        LineAndPointFormatter series1Format = new LineAndPointFormatter(
-                Color.BLUE,                   // line color
-                Color.rgb(0, 100, 0));
-
-        mySimpleXYPlot.addSeries(series1, series1Format);
-        mySimpleXYPlot.setTitle((String) definition.get(C.WORKOUT_NAME));
-
-        mySimpleXYPlot.setDomainValueFormat( new FptDateFormat());
-
-        mySimpleXYPlot.disableAllMarkup();
-
-
+    for (Storable storable : entries) {
+      Workout workout = (Workout) storable;
+      yValues.add((Number) workout.get(xValuePropName));
+      xValues.add(workout.getCreated().getTime());
     }
+
+    mySimpleXYPlot.setBackgroundColor(Color.TRANSPARENT);
+
+    mySimpleXYPlot.setDrawBorderEnabled(false);
+
+    XYSeries series1 = new SimpleXYSeries(
+        xValues,
+        yValues,
+        "");
+    // Create a formatter to use for drawing a series using LineAndPointRenderer:
+    LineAndPointFormatter series1Format = new LineAndPointFormatter(
+        Color.BLUE,                   // line color
+        Color.rgb(0, 100, 0));
+
+    mySimpleXYPlot.addSeries(series1, series1Format);
+    mySimpleXYPlot.setTitle((String) definition.get(C.WORKOUT_NAME));
+
+    mySimpleXYPlot.setDomainValueFormat(new FptDateFormat());
+
+    mySimpleXYPlot.disableAllMarkup();
+
+
+  }
 
 }
