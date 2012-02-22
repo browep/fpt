b569c34c066a2798d78431c73d0632670406e868
paul <paul@paul-Pangolin-Performance.(none)>
2012-02-21 21:42:25 -0700

    viewpager for picture view

--- src/com/github/browep/fpt/ui/SeePictures.java
+++ src/com/github/browep/fpt/ui/SeePictures.java
@@ -1,12 +1,19 @@
 package com.github.browep.fpt.ui;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Parcelable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.*;
 import android.view.animation.AnimationUtils;
 import android.widget.*;
@@ -17,229 +24,279 @@ import com.github.browep.fpt.util.Log;
 import com.github.browep.fpt.util.Util;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.*;
 
-/**
- * Created by IntelliJ IDEA.
- * User: paul
- * Date: 3/12/11
- * Time: 3:00 PM
- * To change this template use File | Settings | File Templates.
- */
-public class SeePictures extends DaoAwareActivity implements ViewSwitcher.ViewFactory {
+public class SeePictures extends DaoAwareActivity implements ViewPager.OnPageChangeListener{
 
-  private static final String MORE_PICTURES_MESSAGE = "Take more pictures!";
-  private Bitmap nextBitmap;
-  private Bitmap previousBitmap;
-  private ImageSwitcher mSwitcher;
-  private Button previousButton;
-  private Button nextButton;
-  private Integer index = 0;
-  private List<File> picturesList;
-  private Display display;
-  private SeePictures self = this;
-  private Bitmap currentBitmap;
-  private String currentFileName;
+    private static final String MORE_PICTURES_MESSAGE = "Take more pictures!";
+    private Bitmap nextBitmap;
+    private Bitmap previousBitmap;
+    private Button previousButton;
+    private Button nextButton;
+    private Integer index = 0;
+    private List<File> picturesList;
+    private SeePictures self = this;
+    private ViewPager viewPager;
 
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
 
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.see_pictures);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.see_pictures);
 
-    previousButton = (Button) findViewById(R.id.previous_button);
-    nextButton = (Button) findViewById(R.id.next_button);
+        previousButton = (Button) findViewById(R.id.previous_button);
+        nextButton = (Button) findViewById(R.id.next_button);
 
-    previousButton.setOnClickListener(previousButtonOnClickListener);
-    nextButton.setOnClickListener(nextButtonOnClickListener);
+        previousButton.setOnClickListener(previousButtonOnClickListener);
+        nextButton.setOnClickListener(nextButtonOnClickListener);
 
 
-    mSwitcher = (ImageSwitcher) findViewById(R.id.switcher);
-    mSwitcher.setFactory(this);
-    mSwitcher.setInAnimation(AnimationUtils.loadAnimation(this,
-        android.R.anim.fade_in));
-    mSwitcher.setOutAnimation(AnimationUtils.loadAnimation(this,
-        android.R.anim.fade_out));
+        // get the earliest picture
 
-    // get the earliest picture
+        File pictureDirectory = new File(Util.getThumbsDirectory());
+        File[] pictures = pictureDirectory.listFiles();
 
-    File pictureDirectory = new File(Util.getThumbsDirectory());
-    File[] pictures = pictureDirectory.listFiles();
+        if (pictures.length > 0) {
+            picturesList = new LinkedList<File>();
+            for (File pictureFile : pictures)
+                picturesList.add(pictureFile);
 
-    if (pictures.length > 0) {
-      picturesList = new LinkedList<File>();
-      for (File pictureFile : pictures)
-        picturesList.add(pictureFile);
+            Collections.sort(picturesList, new Comparator<File>() {
+                public int compare(File file, File file1) {
+                    return file.getAbsoluteFile().compareTo(file1.getAbsoluteFile());
+                }
+            });
 
-      Collections.sort(picturesList, new Comparator<File>() {
-        public int compare(File file, File file1) {
-          return file.getAbsoluteFile().compareTo(file1.getAbsoluteFile());
+            File pictureFile = picturesList.get(index);
+            updateTextDislay(pictureFile);
+
+            viewPager = (ViewPager) findViewById(R.id.view_pager);
+            viewPager.setAdapter(new PicturePagerAdapter());
+            viewPager.setOnPageChangeListener(this);
+
+        } else {
+            Util.longToastMessage(this, "Need to add pictures first. Click \"Add a Progress Picture\" on the welcome screen first");
+            finish();
         }
-      });
 
-      File pictureFile = picturesList.get(index);
-      display = getWindowManager().getDefaultDisplay();
-      mSwitcher.setImageDrawable(new BitmapDrawable(pictureFile.getAbsolutePath()));
-      updateTextDislay(pictureFile);
 
-    } else {
-      Util.longToastMessage(this, "Need to add pictures first. Click \"Add a Progress Picture\" on the welcome screen first");
-      finish();
+        getFptApplication().getTracker().trackEvent(
+                "Picture",  // Category
+                "Viewed",  // Action
+                null, // Label
+                0);
+
     }
 
 
-    getFptApplication().getTracker().trackEvent(
-           "Picture",  // Category
-           "Viewed",  // Action
-          null, // Label
-           0 );
+    View.OnClickListener nextButtonOnClickListener = new View.OnClickListener() {
+        public void onClick(View view) {
+            if (picturesList.size() > 1) {
 
-  }
+                if (viewPager.getCurrentItem() < picturesList.size() - 1) {
+                    viewPager.setCurrentItem(viewPager.getCurrentItem()+1,true);
+                }
+
+            } else {
+                Util.longToastMessage(self, MORE_PICTURES_MESSAGE);
+            }
+
+        }
+    };
 
 
-  View.OnClickListener nextButtonOnClickListener = new View.OnClickListener() {
-    public void onClick(View view) {
-      if (picturesList.size() > 1) {
+    View.OnClickListener previousButtonOnClickListener = new View.OnClickListener() {
+        public void onClick(View view) {
+            if (picturesList.size() > 1) {
 
-        mSwitcher.setImageDrawable(new BitmapDrawable(nextBitmap));
+                if (viewPager.getCurrentItem() > 0) {
+                    viewPager.setCurrentItem(viewPager.getCurrentItem()-1,true);
+                }
 
-        if (index == picturesList.size() - 1)
-          index = 0;
-        else
-          index += 1;
+            } else {
+                Util.longToastMessage(self, MORE_PICTURES_MESSAGE);
+            }
+        }
+    };
 
-        File file = picturesList.get(index);
+
+    private void updateTextDislay(File file) {
+        try {
+            String name = file.getName().replace(".jpg", "");
+            Date date = new Date(Long.parseLong(name));
+            int dateDelta = (int) (((new Date()).getTime() - date.getTime()) / C.MILLIS_IN_A_DAY);
+            TextView pictureTitle = (TextView) findViewById(R.id.picture_title);
+            String title = dateDelta == 0 ? "Today" : dateDelta + " day" + (dateDelta == 1 ? "" : "s") + " ago";
+            pictureTitle.setText(title);
+        } catch (Exception e) {
+            Log.e("error with " + file != null ? file.getAbsolutePath() : "null file", e);
+        }
+
+    }
+
+    public View makeView() {
+        ImageView i = new ImageView(this);
+        i.setBackgroundColor(0xFF000000);
+        i.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        i.setLayoutParams(new ImageSwitcher.LayoutParams(Gallery.LayoutParams.MATCH_PARENT,
+                Gallery.LayoutParams.MATCH_PARENT));
+        return i;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuItem item = menu.add("Delete Picture");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // open up the edit screen
+        AlertDialog.Builder builder = new AlertDialog.Builder(self);
+        builder.setCancelable(true);
+        builder.setTitle("Confirm Delete");
+        builder.setInverseBackgroundForced(true);
+        builder.setMessage("Are you sure you want to delete this picture?  There is no UNDO!");
+
+        builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                File currentFileThumb = picturesList.get(viewPager.getCurrentItem());
+                Log.i("deleting " + currentFileThumb.getAbsolutePath());
+                currentFileThumb.delete();
+                // get saved file
+                File currentFile = new File(currentFileThumb.getAbsolutePath().replace("/thumbs", ""));
+                Log.i("deleting " + currentFile.getAbsolutePath());
+                currentFile.delete();
+
+                Util.longToastMessage(self, "Picture Deleted.");
+                finish();
+
+            }
+        });
+
+        builder.setNegativeButton("Don't Delete", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
+        return true;
+    }
+
+
+    @Override
+    public String getPageName() {
+        return "SeePictures";
+    }
+
+    public void onPageScrolled(int i, float v, int i1) {
+
+    }
+
+    public void onPageSelected(int i) {
+        File file = picturesList.get(i);
         Log.i("setting to file " + file.getName());
-        currentFileName = file.getAbsolutePath();
-
-
         updateTextDislay(file);
-
-        setupNextImages(index);
-
-      } else {
-        Util.longToastMessage(self, MORE_PICTURES_MESSAGE);
-      }
-
-    }
-  };
-
-
-  View.OnClickListener previousButtonOnClickListener = new View.OnClickListener() {
-    public void onClick(View view) {
-      if (picturesList.size() > 1) {
-
-
-        mSwitcher.setImageDrawable(new BitmapDrawable(previousBitmap));
-        if (index == 0)
-          index = picturesList.size() - 1;
-        else
-          index -= 1;
-
-        File file = picturesList.get(index);
-        Log.i("setting to file " + file.getName());
-
-        currentFileName = file.getAbsolutePath();
-
-        updateTextDislay(file);
-
-        setupNextImages(index);
-
-
-      } else {
-        Util.longToastMessage(self, MORE_PICTURES_MESSAGE);
-      }
-    }
-  };
-
-
-  private void updateTextDislay(File file) {
-    try {
-      String name = file.getName().replace(".jpg", "");
-      Date date = new Date(Long.parseLong(name));
-      int dateDelta = (int) (((new Date()).getTime() - date.getTime()) / C.MILLIS_IN_A_DAY);
-      TextView pictureTitle = (TextView) findViewById(R.id.picture_title);
-      String title = dateDelta == 0 ? "Today" : dateDelta + " day" + (dateDelta == 1 ? "" : "s") + " ago";
-      pictureTitle.setText(title);
-    } catch (Exception e) {
-      Log.e("error with " + file != null ? file.getAbsolutePath() : "null file", e);
     }
 
-  }
+    public void onPageScrollStateChanged(int i) {
 
-  @Override
-  protected void onPostCreate(Bundle savedInstanceState) {
-    super.onPostCreate(savedInstanceState);
-
-    setupNextImages(0);
-  }
-
-  private void setupNextImages(Integer currentIndex) {
-    if (picturesList.size() > 1) {
-
-      Integer nextIndex = currentIndex == picturesList.size() - 1 ? 0 : currentIndex + 1;
-      Integer previousIndex = currentIndex == 0 ? picturesList.size() - 1 : currentIndex - 1;
-
-      nextBitmap = BitmapFactory.decodeFile(picturesList.get(nextIndex).getAbsolutePath());
-      previousBitmap = BitmapFactory.decodeFile(picturesList.get(previousIndex).getAbsolutePath());
     }
 
+    private class PicturePagerAdapter extends PagerAdapter {
 
-  }
+        public PicturePagerAdapter() {
 
-  public View makeView() {
-    ImageView i = new ImageView(this);
-    i.setBackgroundColor(0xFF000000);
-    i.setScaleType(ImageView.ScaleType.FIT_CENTER);
-    i.setLayoutParams(new ImageSwitcher.LayoutParams(Gallery.LayoutParams.MATCH_PARENT,
-        Gallery.LayoutParams.MATCH_PARENT));
-    return i;
-  }
+        }
 
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    MenuItem item = menu.add("Delete Picture");
-    return true;
-  }
+        @Override
+        public int getCount() {
+            return picturesList.size();
+        }
 
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // open up the edit screen
-     AlertDialog.Builder builder = new AlertDialog.Builder(self);
-    builder.setCancelable(true);
-    builder.setTitle("Confirm Delete");
-    builder.setInverseBackgroundForced(true);
-    builder.setMessage("Are you sure you want to delete this picture?  There is no UNDO!");
+        /**
+         * Create the page for the given position.  The adapter is responsible
+         * for adding the view to the container given here, although it only
+         * must ensure this is done by the time it returns from
+         * {@link #finishUpdate()}.
+         *
+         * @param container The containing View in which the page will be shown.
+         * @param position The page position to be instantiated.
+         * @return Returns an Object representing the new page.  This does not
+         * need to be a View, but can be some other container of the page.
+         */
+        @Override
+        public Object instantiateItem(View collection, int position) {
 
-    builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
-      public void onClick(DialogInterface dialog, int which) {
-        File currentFileThumb = new File(currentFileName);
-        Log.i("deleting " + currentFileThumb.getAbsolutePath());
-        currentFileThumb.delete();
-        // get saved file
-        File currentFile = new File(currentFileName.replace("/thumbs",""));
-        Log.i("deleting " + currentFile.getAbsolutePath());
-        currentFile.delete();
+            ImageView imageView = new ImageView(SeePictures.this);
 
-        Util.longToastMessage(self,"Picture Deleted.");
-        finish();
+            String absolutePath = picturesList.get(position).getAbsolutePath();
 
-      }
-    });
+            Log.d("instantiating " + position + " at " + absolutePath);
 
-    builder.setNegativeButton("Don't Delete", new DialogInterface.OnClickListener() {
-      public void onClick(DialogInterface dialog, int which) {
-        dialog.dismiss();
-      }
-    });
-    AlertDialog alert = builder.create();
-    alert.show();
-    return true;
-  }
+            imageView.setImageDrawable(new BitmapDrawable(absolutePath));
+
+            ((ViewPager) collection).addView(imageView, 0);
 
 
-  @Override
-  public String getPageName() {
-    return "SeePictures";
-  }
+            return imageView;
+        }
+
+        /**
+         * Remove a page for the given position.  The adapter is responsible
+         * for removing the view from its container, although it only must ensure
+         * this is done by the time it returns from {@link #finishUpdate()}.
+         *
+         * @param container The containing View from which the page will be removed.
+         * @param position The page position to be removed.
+         * @param object The same object that was returned by
+         * {@link #instantiateItem(View, int)}.
+         */
+        @Override
+        public void destroyItem(View collection, int position, Object view) {
+
+            ImageView imageView = (ImageView) view;
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) imageView.getDrawable();
+            if (bitmapDrawable != null) {
+                Bitmap bitmap = bitmapDrawable.getBitmap();
+                if(bitmap != null){
+                    bitmap.recycle();
+                }
+            }
+            ((ViewPager) collection).removeView(imageView);
+        }
+
+
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view==(object);
+        }
+
+
+        /**
+         * Called when the a change in the shown pages has been completed.  At this
+         * point you must ensure that all of the pages have actually been added or
+         * removed from the container as appropriate.
+         * @param container The containing View which is displaying this adapter's
+         * page views.
+         */
+        @Override
+        public void finishUpdate(View arg0) {}
+
+
+        @Override
+        public void restoreState(Parcelable arg0, ClassLoader arg1) {}
+
+        @Override
+        public Parcelable saveState() {
+            return null;
+        }
+
+        @Override
+        public void startUpdate(View arg0) {}
+
+    }
 }
